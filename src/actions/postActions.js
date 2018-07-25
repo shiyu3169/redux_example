57051c2164702a7fc56138b291e24755513dee2e
Shiyu Wang <shiyu3169@gmail.com>
2018-07-25 10:22:42 -0400

    adding new post

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,4 +1,4 @@
-import { FETCH_POSTS, NEW_POSTS } from './types';
+import { FETCH_POSTS, NEW_POST } from './types';
 
 export const fetchPosts = () => dispatch => {
 	// console.log("fetching");
@@ -10,4 +10,21 @@ export const fetchPosts = () => dispatch => {
 			payload: posts
 		})
 	);
+}
+
+export const createPost = (postData) => dispatch => {
+	// console.log("action called")
+	fetch('https://jsonplaceholder.typicode.com/posts', {
+		method: 'POST',
+		headers: {
+			'content-type': 'application/json'
+		},
+			body: JSON.stringify(postData)
+		}).then(
+			res => res.json()
+		). then (
+			post => dispatch({
+			type: NEW_POST,
+			payload: postData
+		}));
 }
--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,4 +1,4 @@
-import { FETCH_POSTS, NEW_POSTS } from '../actions/types';
+import { FETCH_POSTS, NEW_POST } from '../actions/types';
 
 const initialState = {
 	items: [],
@@ -12,7 +12,12 @@ export default function(state = initialState, action) {
 			return {
 				...state,
 				items: action.payload
-			}
+			};
+		case NEW_POST:
+			return {
+				...state,
+				item: action.payload
+			};
 		default:
 			return state;
 	}
